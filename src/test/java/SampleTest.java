c85e0a70d0a8f95bea050e3a95eff7dfca553c99
Azfar Hussain <81310231+azfarhussain-10p@users.noreply.github.com>
2022-09-30 17:22:26 +0500

    L02- Working with Configuration Property File

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,3 +1,4 @@
+import configurationFileReader.ConfigReader;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
@@ -10,17 +11,23 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class SampleTest {
+
+    ConfigReader configReader;
+
+    public SampleTest() {
+        configReader = new ConfigReader();
+    }
     AppiumDriver driver;
 
     @BeforeTest
     public void initializer() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "8.1");
-        capabilities.setCapability("deviceName", "emulator-5554");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("app", System.getProperty("user.dir") + "/apps/ApiDemos.apk");
-        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
+        capabilities.setCapability("platformName", configReader.getPlatformName());
+        capabilities.setCapability("platformVersion", configReader.getPlatformVersion());
+        capabilities.setCapability("deviceName", configReader.getDeviceName());
+        capabilities.setCapability("automationName", configReader.getAutomationName());
+        capabilities.setCapability("app", System.getProperty("user.dir") + configReader.getApkPath());
+        driver = new AndroidDriver(new URL(configReader.appiumServerEndpointURL()), capabilities);
         System.out.println("Opening Appium Server");
     }
 
