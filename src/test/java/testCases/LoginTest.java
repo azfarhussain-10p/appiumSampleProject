c3304d1ca8b3fc80659d8f59acf79ab1150aa89e
Azfar Hussain <81310231+azfarhussain-10p@users.noreply.github.com>
2022-10-25 16:26:41 +0500

    Integrated Reporting Framework

--- src/test/java/testCases/LoginTest.java
+++ src/test/java/testCases/LoginTest.java
@@ -1,6 +1,7 @@
 package testCases;
 
 import base.AppFactory;
+import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -22,6 +23,12 @@ public class LoginTest {
     }
 
     @Test
+    @Flaky
+    @Epic("Booking.com")
+    @Feature("Account Creation")
+    @Description("From this Test Case User should able to create a New Account")
+    @Story("When user should create a New Account for Login into the application")
+    @Severity(SeverityLevel.BLOCKER)
     public void verifyUserCreation() throws InterruptedException {
         loginPage.clickCreateAccountButton();
         Assert.assertTrue(loginPage.pageHeader.isDisplayed());
@@ -35,6 +42,52 @@ public class LoginTest {
         loginPage.clickSignInButton();
         System.out.println("Creating New User Account");
         Thread.sleep(1000);
+    }
+
+    @Test
+    @Epic("Booking.com")
+    @Feature("Login")
+    @Description("From this Test Case User is able to login")
+    @Story("When user should able to login into the application")
+    @Severity(SeverityLevel.BLOCKER)
+    public void sampleTestOne(){
+        System.out.println("This is a Sample Test One");
+    }
+    @Test
+    @Epic("Booking.com")
+    @Feature("Forget Password")
+    @Description("From this Test Case User should able reset new Password")
+    @Story("When user should to reset New Password form application")
+    @Severity(SeverityLevel.CRITICAL)
+    public void sampleTestTwo(){
+        System.out.println("This is a Sample Test Two");
+    }
+    @Test
+    @Epic("Booking.com")
+    @Feature("Log Out")
+    @Description("From this Test Case User should able to logout")
+    @Story("When user should Logout form the application")
+    @Severity(SeverityLevel.CRITICAL)
+    public void sampleTestThree(){
+        System.out.println("This is a Sample Test Three");
+    }
+    @Test
+    @Epic("Booking.com")
+    @Feature("Delete User")
+    @Description("From this Test Case User should able to Delete Account")
+    @Story("When user should delete previous user account form the application")
+    @Severity(SeverityLevel.CRITICAL)
+    public void sampleTestFour(){
+        System.out.println("This is a Sample Test Four");
+    }
+    @Test
+    @Epic("Booking.com")
+    @Feature("Push Notification")
+    @Description("From this Test Case User should able to see the Push Notification")
+    @Story("When user should see the Push Notification after deleting account")
+    @Severity(SeverityLevel.MINOR)
+    public void sampleTestFive(){
+        System.out.println("This is a Sample Test Five");
     }
 
     @Test
